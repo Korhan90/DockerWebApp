89a2825ec485fc91177ed30e9ee0b68dfae416ab
Test <test@test.com>
2021-07-29 20:23:01 +0300

    Add project files.

---
+++ DockerWebApp/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using DockerWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.Xml;
+using System.Threading.Tasks;
+
+namespace DockerWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        static readonly HttpClient client = new HttpClient();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+          
+
+        }
+        //TEST
+        public async Task<IActionResult> Index()
+        {
+            // string url = "http://172.20.0.2/WeatherForecast";
+            //   string url = "http://myapp.dev/WeatherForecast";
+            string url = "http://serviceapp/WeatherForecast";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                ViewBag.response = responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                ViewBag.response = JsonConvert.SerializeObject(e);
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
